08d561f18084c39476e8b45643707d56aa83d8d3
diyaacharya <55196217+diyaacharya@users.noreply.github.com>
2020-08-18 12:23:12 +0530

    Add files via upload

---
+++ day1/day1_arrary.py
@@ -0,0 +1,17 @@
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    nd = input().split()
+
+    n = int(nd[0])
+
+    d = int(nd[1])
+
+    a = list(map(int, input().rstrip().split()))
+    for rotate in (a[d:] + a[0:d]): print (rotate ,end=" ")
---
+++ day1/day1_arrays.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+
+    
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int d = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i<n;i++) 
+        {
+            arr[(i+n-d)%n] = sc.nextInt();
+        }
+        for(int i=0; i<n;i++) 
+        {
+            System.out.print(arr[i] + " ");
+        }      
+    }
+}
